e6f41c068ce41acfd0b6ffc5bf7b433147f3ee32
Erin <79354991+oati@users.noreply.github.com>
2023-10-06 08:55:41 -0500

    inital commit

---
+++ flake.nix
@@ -0,0 +1,27 @@
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    flake-parts.url = "github:hercules-ci/flake-parts";
+  };
+
+  outputs = inputs@{ flake-parts, ... }:
+    flake-parts.lib.mkFlake { inherit inputs; } {
+      systems = [ "x86_64-linux" "aarch64-linux" "x86_64-darwin" "aarch64-darwin" ];
+
+      perSystem = { pkgs, ... }: {
+        devShells.default = pkgs.mkShell {
+          buildInputs = with pkgs; [
+            (python311.withPackages (p: with p; [
+              jax
+              equinox
+
+              python-lsp-server
+              python-lsp-ruff
+              python-lsp-black
+              pylsp-mypy
+            ]))
+          ];
+        };
+      };
+    };
+}
