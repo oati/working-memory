db1d53ab5fb322487aefe35bb9b7ea9c02c6d86a
Erin <79354991+oati@users.noreply.github.com>
2023-12-29 00:13:40 -0600

    update test.py

--- test.py
+++ test.py
@@ -1,5 +1,6 @@
 import jax
 import jax.numpy as jnp
+
 from model import (
     Model,
     ModelHyperparameters,
@@ -21,6 +22,40 @@ from hyperparameters import (
 )
 
 
+def simulate_model(model, params, state, steps, dt, key):
+    yield params, state
+    for _ in range(steps):
+        key, new_key = jax.random.split(key)
+        params, state = model(params, state, jnp.array([1]), jnp.ones((2, 3)), new_key)
+        yield params, state
+
+
+def filter_data(params, state):
+    return {
+        "parameters": {
+            "w_l1_l1": params.w_l1_l1,
+            "k_l2_l2": params.k_l2_l2,
+            "a_l2_l2": params.a_l2_l2,
+            "k_l3_l3": params.k_l3_l3,
+            "a_l3_l3": params.a_l3_l3,
+            "w_l2_l3": params.w_l2_l3,
+        },
+        "pyramidal_firing_rates": {
+            "wm_layer": state.wm_layer.pyramidal_firing_rate,
+            "layer1": state.layer1.pyramidal_firing_rate,
+            "layer2": state.layer2.pyramidal_firing_rate,
+            "layer3": state.layer3.pyramidal_firing_rate,
+        },
+    }
+
+
+def model_data(*args):
+    return jax.tree_map(
+        lambda *x: jnp.stack(x, axis=-1),
+        *(filter_data(params, state) for params, state in simulate_model(*args))
+    )
+
+
 if __name__ == "__main__":
     key = jax.random.PRNGKey(0)
     model = Model(
@@ -35,12 +70,7 @@ if __name__ == "__main__":
     state = model.init_state(jnp.array([0]))
     # state = column.init_state(jnp.array(0))
 
-    t = 0.0
-    while t < 1:
-        t += dt
-        key, new_key = jax.random.split(key)
-        # state = column(
-        #     state, jnp.array(10), jnp.array(0), jnp.array(0), jnp.array(0), new_key
-        # )
-        params, state = model(params, state, jnp.array([0]), jnp.zeros((2, 3)), new_key)
-    print(state)
+    # t = jnp.arange(0, 1, dt)
+    # data = model_data(model, params, state, 1, dt, key)
+    data = model_data(model, params, state, 100, dt, key)
+    print(data["pyramidal_firing_rates"]["wm_layer"])
